3e8abc63c60333fa7f8a595dbceb93c034b34be9
Nataniel <natanielarg@gmail.com>
2022-04-24 06:36:00 -0300

    punto5-commit1-clase Principal creada y desarrollada

--- TP3-Grupo16/src/main/java/ar/edu/unju/fi/tp3/punto5/Principal.java
+++ TP3-Grupo16/src/main/java/ar/edu/unju/fi/tp3/punto5/Principal.java
@@ -1,10 +1,21 @@
 package ar.edu.unju.fi.tp3.punto5;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Ingrese una cadena de texto: ");
+		String cad = scan.next();
+		
+		char[] cadC = cad.toCharArray();
+		
+		System.out.print("Cadena invertida: ");
+		for(int i = cadC.length-1;i >= 0;i--) {
+			System.out.print(cadC[i]);
+		}
+		scan.close();
 	}
-
 }
