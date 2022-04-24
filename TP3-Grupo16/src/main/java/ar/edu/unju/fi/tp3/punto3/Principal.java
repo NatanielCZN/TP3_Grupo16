10762e353f24c42b82b931ae56ab4f01204f35c3
Nataniel <natanielarg@gmail.com>
2022-04-24 18:19:41 -0300

    punto3-commit1-clases Alumno y Princial creadas y desarrolladas

--- TP3-Grupo16/src/main/java/ar/edu/unju/fi/tp3/punto3/Principal.java
+++ TP3-Grupo16/src/main/java/ar/edu/unju/fi/tp3/punto3/Principal.java
@@ -1,10 +1,57 @@
 package ar.edu.unju.fi.tp3.punto3;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		Alumno alumno = new Alumno();
+		System.out.println("Ingrese datos de un alumno: ");
+		
+		System.out.print("Nombre: ");
+		String nom = scan.next();		
+		alumno.setNombre(nom);
+		
+		System.out.print("Apellido: ");
+		String ape = scan.next();
+		alumno.setApellido(ape);
+		
+		boolean band;
+		do {
+			band = false;
+			try {
+				System.out.println("Libreta Universitaria: ");
+				int lu = scan.nextInt();
+				alumno.setLu(lu);
+				band = true;
+			}catch(InputMismatchException ie) {
+				System.out.println("Ocurrio un error -> " + ie.getMessage());
+				scan.nextLine();
+			}
+		}while(band == false);
+		
+		double[] notas = new double[alumno.getNotas().length];
+		System.out.println("Notas de materias: ");
+		for(int i = 0;i < notas.length;i++) {
+			do {
+				band = false;
+				try {
+					System.out.print("Nota " + (i+1) + ": ");
+					double not = scan.nextDouble();
+					notas[i] = not;
+					band = true;
+				}catch(InputMismatchException ie) {
+					System.out.println("Ocurrio un error -> " + ie.getMessage());
+					scan.nextLine();
+				}
+			}while(band == false);						
+		}
+		alumno.setNotas(notas);
+		
+		System.out.println("Promedio de notas: " + alumno.calcularPromedio());
+		System.out.println("Nota mas alta: " + alumno.obtenerNotaMaxima());
 	}
-
 }
